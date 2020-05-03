7d7db24fb978ad5b8e983772411ea8c042a74561
hripsimeg <hripsimeg@intermotionllc.com>
2020-05-03 19:31:40 +0400

    level_up_1

--- app/src/main/java/com/example/backendexperiments/MainActivity.java
+++ app/src/main/java/com/example/backendexperiments/MainActivity.java
@@ -7,6 +7,10 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ListView;
 
 import com.android.volley.Request;
@@ -18,30 +22,72 @@ import com.example.backendexperiments.Util.Parser;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    String urlString = "https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=ryOX7WaLMNT9uclXL53TkYHMcYkQTYFa";
+public class MainActivity extends AppCompatActivity {//lists/overview/2013-05-22
+//    String urlString = "https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=ryOX7WaLMNT9uclXL53TkYHMcYkQTYFa";
+    String urlString;
     MyRequestQueue myQueue;
     Context context;
     ListView listView;
+    ListAdapter adapter;
     List<BooksListsModel> booksListsModelList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = findViewById(R.id.listView);
         context = getApplicationContext();
         myQueue = MyRequestQueue.getInstance(context);
+//        MakeRequest request = new MakeRequest(context);
+//        request.execute();
+        final DatePicker simpleDatePicker = findViewById(R.id.simpleDatePicker);
+        simpleDatePicker.setVisibility(View.INVISIBLE);
+        listView = findViewById(R.id.listViewId);
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                simpleDatePicker.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            }
+        });
+        Button selectButton = findViewById(R.id.selectButtonId);
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                simpleDatePicker.setVisibility(View.VISIBLE);
+            }
+        });
+        Button okButton = findViewById(R.id.okButtonId);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDate(simpleDatePicker);
+                simpleDatePicker.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    public void getDate(DatePicker simpleDatePicker) {
+        String year = simpleDatePicker.getYear() + "";
+        String month = simpleDatePicker.getMonth() + "";
+        String day = simpleDatePicker.getDayOfMonth() + "";
+        if(Integer.parseInt(month) < 10) month = "0" + month;
+        if(Integer.parseInt(day) < 10) day  = "0" + day ;
+        urlString = "https://api.nytimes.com/svc/books/v3/lists/overview/" + year + "-" + month + "-" + day + ".json?api-key=ryOX7WaLMNT9uclXL53TkYHMcYkQTYFa";
+        Log.d("ExperimentsLog", "getDate: url = " + urlString);
         MakeRequest request = new MakeRequest(context);
         request.execute();
     }
 
-
     public void createListStuff() {
-        ListAdapter adapter = new ListAdapter(context, R.layout.simple_list_item, booksListsModelList/*new String[]{"Lion", "Tiger", "Monkey"}*/);
+        if (adapter == null) adapter = new ListAdapter(context, R.layout.simple_list_item, booksListsModelList/*new String[]{"Lion", "Tiger", "Monkey"}*/);
         listView.setAdapter(adapter);
 
     }
+    
     public  class MakeRequest extends AsyncTask<String, Void, String> {
         Context context;
         MakeRequest(Context context) {
@@ -54,14 +100,15 @@ public class MainActivity extends AppCompatActivity {
                 @SuppressLint("SetTextI18n")
                 @Override
                 public void onResponse(String response) {
-                    booksListsModelList = new Parser().parseLists(response);
+//                    booksListsModelList = new Parser().parseLists(response);
+                    booksListsModelList = new Parser().parseListsWithDate(response);
                     createListStuff();
                 }
             }, new Response.ErrorListener() {
                 @SuppressLint("SetTextI18n")
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Log.d("request", "doInBackground: error = " + error);
+                    Log.d("ExperimentsLog", "doInBackground: error = " + error);
                 }
             });
             myQueue.addToRequestQueue(request);
--- app/src/main/java/com/example/backendexperiments/Util/Parser.java
+++ app/src/main/java/com/example/backendexperiments/Util/Parser.java
@@ -1,5 +1,8 @@
 package com.example.backendexperiments.Util;
 
+import android.util.Log;
+
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.backendexperiments.BooksListsModel;
 
 import org.json.JSONArray;
@@ -35,10 +38,48 @@ public class Parser {
                     model.setNewest_published_date(obj.getString("newest_published_date"));
                 if (obj.has("updated"))
                     model.setUpdated(obj.getString("updated"));
-                if (model != null)
-                    booksListsModels.add(model);
+                booksListsModels.add(model);
             }
         } catch (JSONException e) {
+            Log.d("ExperimentsLog", "parseLists: error = " + e);
+            e.printStackTrace();
+        }
+        return booksListsModels;
+    }
+
+    public List<BooksListsModel> parseListsWithDate(String response) {
+        JSONObject jsonObject;
+        JSONArray jsonArray;
+        BooksListsModel model;
+        List<BooksListsModel> booksListsModels = new ArrayList<>();
+
+        try {
+            jsonObject = new JSONObject(response);
+            JSONObject object0 = jsonObject.getJSONObject("results");
+            jsonArray = object0.getJSONArray("lists");
+
+            JSONObject object = (JSONObject) jsonArray.get(0);
+            jsonArray = (JSONArray) object.get("books");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                model = new BooksListsModel();
+                JSONObject obj = jsonArray.getJSONObject(i);
+                if (obj.has("author"))
+                    if (obj.has("title"))
+                        model.setUpdated(obj.getString("title"));
+                    model.setListName(obj.getString("author"));
+                if (obj.has("contributor"))
+                    model.setDisplayName(obj.getString("contributor"));
+                if (obj.has("created_date"))
+                    model.setListNameEncoded(obj.getString("created_date"));
+                if (obj.has("description"))
+                    model.setOldest_published_date(obj.getString("description"));
+                if (obj.has("price"))
+                    model.setNewest_published_date(obj.getString("price"));
+                booksListsModels.add(model);
+            }
+        } catch (JSONException e) {
+            Log.d("ExperimentsLog", "parseLists: error = " + e);
             e.printStackTrace();
         }
         return booksListsModels;
